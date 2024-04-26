9eacd65f60d0895a62b2c68a0b87fcf015700e8e
VeyronSakai <yuukisakai123@gmail.com>
2024-04-27 00:18:59 +0900

    Add iOS Build Job

--- Assets/Editor/BuildEntryPoint.cs
+++ Assets/Editor/BuildEntryPoint.cs
@@ -2,15 +2,31 @@ using UnityEditor;
 
 namespace Editor
 {
+    // ReSharper disable once UnusedMember.Global
     public sealed class BuildEntryPoint
     {
-        public static void Build()
+        // ReSharper disable once UnusedMember.Global
+        public static void BuildAndroid()
         {
             var buildPlayerOptions = new BuildPlayerOptions
             {
                 scenes = new[] { "Assets/Scenes/SampleScene.unity" },
                 locationPathName = "Outputs/Android/app.apk",
                 target = BuildTarget.Android,
+                options = BuildOptions.Development,
+            };
+
+            BuildPipeline.BuildPlayer(buildPlayerOptions);
+        }
+
+        // ReSharper disable once UnusedMember.Global
+        public static void BuildIOS()
+        {
+            var buildPlayerOptions = new BuildPlayerOptions
+            {
+                scenes = new[] { "Assets/Scenes/SampleScene.unity" },
+                locationPathName = "Outputs/IOS/",
+                target = BuildTarget.iOS,
                 options = BuildOptions.Development,
             };
 
