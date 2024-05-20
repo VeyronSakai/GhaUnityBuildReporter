3b6bb5dbb196b7f5257efd8ba787c824e4afd8af
Yuki Sakai <yuukisakai123@gmail.com>
2024-05-20 23:38:31 +0900

    Add test case

--- Tests/Editor/UnityBuildReportWriterTest.cs
+++ Tests/Editor/UnityBuildReportWriterTest.cs
@@ -14,12 +14,12 @@ namespace GhaUnityBuildReporter.Editor.Tests
             Path.Combine(Directory.GetCurrentDirectory(), "ActualUnityBuildReport.md");
 
         [Test]
-        public void WriteTest()
+        public void Write_ConfigDoesNotExist_AllInfoIsWritten()
         {
             // Arrange
             var jobSummaryRepository = new FakeJobSummaryRepository(_outputPath);
             var buildReportRepository = new StubBuildReportRepository();
-            var reportConfigRepository = new StubReportConfigRepository();
+            var reportConfigRepository = new StubReportConfigRepository(existsConfig: false);
             var writer =
                 new UnityBuildReportWriter(jobSummaryRepository, buildReportRepository, reportConfigRepository);
 
@@ -30,6 +30,56 @@ namespace GhaUnityBuildReporter.Editor.Tests
             var actual = File.ReadAllText(_outputPath);
             var expected = Helper.GetExpectedResult("ExpectedUnityBuildReport.md");
             Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Write_OnlyTitleAndBasicInfoAreEnabled_OnlyTitleAndBasicInfoAreWritten()
+        {
+            // Arrange
+            var jobSummaryRepository = new FakeJobSummaryRepository(_outputPath);
+            var buildReportRepository = new StubBuildReportRepository();
+            var reportConfigRepository = new StubReportConfigRepository(
+                writesTitle: true,
+                writesBasicInfo: true,
+                writesBuildSteps: false,
+                writesSourceAssets: false,
+                writesOutputFiles: false,
+                writesIncludedModules: false
+            );
+            var writer =
+                new UnityBuildReportWriter(jobSummaryRepository, buildReportRepository, reportConfigRepository);
+
+            // Act
+            writer.Write();
+
+            // Assert
+            var actual = File.ReadAllText(_outputPath);
+            var expected = Helper.GetExpectedResult("ExpectedUnityBuildReport_OnlyTitleAndBasicInfoAreWritten.md");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Write_AllConfigAreDisabled_NothingIsWritten()
+        {
+            // Arrange
+            var jobSummaryRepository = new FakeJobSummaryRepository(_outputPath);
+            var buildReportRepository = new StubBuildReportRepository();
+            var reportConfigRepository = new StubReportConfigRepository(
+                writesTitle: false,
+                writesBasicInfo: false,
+                writesBuildSteps: false,
+                writesSourceAssets: false,
+                writesOutputFiles: false,
+                writesIncludedModules: false
+            );
+            var writer =
+                new UnityBuildReportWriter(jobSummaryRepository, buildReportRepository, reportConfigRepository);
+
+            // Act
+            writer.Write();
+
+            // Assert
+            Assert.IsFalse(File.Exists(_outputPath));
         }
 
         [SetUp, TearDown]
